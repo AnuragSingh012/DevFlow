ee7387b5397cfe0f3a473d70d5f12880d1ea4511
Anurag Singh <anuragsingh4932@gmail.com>
2024-06-16 13:22:15 +0530

    Add New Feature

--- server/index.js
+++ server/index.js
@@ -30,12 +30,13 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production", 
-      httpOnly: true,
-      sameSite: "lax", // Adjust based on your needs ('lax' or 'none')
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days (in milliseconds)
+      sameSite: "lax",
     },
   })
 );
+
 
 
 // Passport local strategy
